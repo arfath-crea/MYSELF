3f1a46a0e91b442c1b40fa9c610cfd70539ae267
arfath-crea <marfath874@gmail.com>
2026-05-20 12:36:29 +0530

    first commit

---
+++ fun.js
@@ -0,0 +1,11 @@
+let pi = 3.14578
+
+let radius;
+let circumstance;
+
+radius = window.prompt("enter the radius of the circle");
+radius = Number(radius);
+
+circumstance = 2 * pi * radius;
+
+console.log(circumstance);
