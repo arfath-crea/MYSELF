bbd7a3c74e46e9bf75dd4aa71ef5e85cd545dd15
arfath-crea <marfath874@gmail.com>
2026-07-24 23:33:46 +0530

    Deploy portfolio to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/MYSELF',
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
